513e3c22b7332befc59747075a5bb39f1a889872
Binco <woongbin96@naver.com>
2022-05-10 19:37:48 +0900

    게시글 업데이트 TDD 연습하기

--- src/test/java/com/binco/book/springboot/web/PostsApiControllerTest.java
+++ src/test/java/com/binco/book/springboot/web/PostsApiControllerTest.java
@@ -3,6 +3,7 @@ package com.binco.book.springboot.web;
 import com.binco.book.springboot.domain.posts.Posts;
 import com.binco.book.springboot.domain.posts.PostsRepository;
 import com.binco.book.springboot.web.dto.PostsSaveRequestDto;
+import com.binco.book.springboot.web.dto.PostsUpdateRequestDto;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,6 +11,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -60,5 +63,39 @@ public class PostsApiControllerTest {
     List<Posts> all = postsRepository.findAll();
     assertThat(all.get(0).getTitle()).isEqualTo(title);
     assertThat(all.get(0).getContent()).isEqualTo(content);
+  }
+
+  @Test
+  public void Posts_수정된다() throws Exception {
+    //given
+    Posts savePosts = postsRepository.save(Posts.builder()
+    .title("init Title")
+    .content("init Content")
+    .author("init Author")
+    .build());
+
+    Long updateId = savePosts.getId();
+    String changeTitle = "update Title";
+    String changeContent = "update Content";
+
+    PostsUpdateRequestDto requestDto = PostsUpdateRequestDto.builder()
+      .title(changeTitle)
+      .content(changeContent)
+      .build();
+
+    String url = "http://localhost:" + port + "/api/v1/posts/" + updateId;
+
+    HttpEntity<PostsUpdateRequestDto> requestEntity = new HttpEntity<>(requestDto);
+
+    //when
+    ResponseEntity<Long> responseEntity = restTemplate.exchange(url, HttpMethod.PUT, requestEntity, Long.class);
+
+    //then
+    assertThat(responseEntity.getStatusCode()).isEqualTo(HttpStatus.OK);
+    assertThat(responseEntity.getBody()).isGreaterThan(0L);
+
+    List<Posts> all = postsRepository.findAll();
+    assertThat(all.get(0).getTitle()).isEqualTo(changeTitle);
+    assertThat(all.get(0).getContent()).isEqualTo(changeContent);
   }
 }
