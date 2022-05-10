19e26d90ba86a37375c996b93824af360e809dfd
Binco <woongbin96@naver.com>
2022-05-10 19:23:07 +0900

    게시글 업데이트 TDD 연습하기

--- src/main/java/com/binco/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/binco/book/springboot/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.binco.book.springboot.service.posts;
 
+import com.binco.book.springboot.domain.posts.Posts;
 import com.binco.book.springboot.domain.posts.PostsRepository;
+import com.binco.book.springboot.web.dto.PostsResponseDto;
 import com.binco.book.springboot.web.dto.PostsSaveRequestDto;
+import com.binco.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,18 @@ public class PostsService {
   @Transactional
   public Long save(PostsSaveRequestDto requestDto){
     return postsRepository.save(requestDto.toEntity()).getId();
+  }
+
+  @Transactional
+  public Long update(Long id, PostsUpdateRequestDto requestDto){
+    Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 게시글이 없습니다. id="+ id));
+
+    posts.update(requestDto.getTitle(), requestDto.getContent());
+
+    return id;
+  }
+
+  public PostsResponseDto findById(Long id) {
+    return null;
   }
 }
