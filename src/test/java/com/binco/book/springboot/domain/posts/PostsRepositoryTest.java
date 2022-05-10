32e02f32dd6a4f45938741021191d23a87713c8c
Binco <woongbin96@naver.com>
2022-05-10 19:52:10 +0900

    BaseTimeEntity 생성 및 적용

--- src/test/java/com/binco/book/springboot/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/binco/book/springboot/domain/posts/PostsRepositoryTest.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -25,7 +27,7 @@ public class PostsRepositoryTest {
   }
 
   @Test
-  public void 게시글저장_불러오기(){
+  public void 게시글저장_불러오기() {
     //given
     String title = "테스트 게시글";
     String content = "테스트 본문";
@@ -43,5 +45,27 @@ public class PostsRepositoryTest {
     Posts posts = postsList.get(0);
     assertThat(posts.getTitle()).isEqualTo(title);
     assertThat(posts.getContent()).isEqualTo(content);
+  }
+
+  @Test
+  public void BaseTimeEntity_등록() {
+    //given
+    LocalDateTime now = LocalDateTime.of(2022,5,10,0,0,0);
+    postsRepository.save(Posts.builder()
+    .title("Now Title")
+    .content("Now Content")
+    .author("Now Author")
+    .build());
+
+    //when
+    List<Posts> postsList = postsRepository.findAll();
+
+    //then
+    Posts posts = postsList.get(0);
+
+    System.out.println(">>>>>> createDate="+posts.getCreatedDate()+", modifiedDate="+posts.getModifiedDate());
+
+    assertThat(posts.getCreatedDate()).isAfter(now);
+    assertThat(posts.getModifiedDate()).isAfter(now);
   }
 }
