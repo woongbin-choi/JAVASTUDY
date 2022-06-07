db79445a788f6dce689ae64ed60966b90369d680
Binco <woongbin96@naver.com>
2022-06-07 12:37:47 +0900

    ACID - Transaction

--- src/main/java/com/developers/dmaker/service/DMakerService.java
+++ src/main/java/com/developers/dmaker/service/DMakerService.java
@@ -14,6 +14,11 @@ import javax.transaction.Transactional;
 public class DMakerService {
   private final DeveloperRepository developerRepository;
 
+  // ACID
+  // Atomic : 원자성 - A 계좌에서 만원 빠져나갔는데, 정전이 나서 B의 계좌에 안들어갈 경우 실패
+  // Consistency : 일관성 - 트랜잭션이 끝나는 시점에는 일관성이 무조건 맞춰있어야 한다(중간 부분은 일관성을 무시해도 됨)
+  // Isolation : 고립성
+  // Durability : 지속성 - 모든 커밋 된 이력을 남긴다
   @Transactional
   public void createdDeveloper(){
     Developer developer = Developer.builder()
