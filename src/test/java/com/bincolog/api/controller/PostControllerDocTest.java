8f7a3ff1c81ac3bd369d315f16bf697309e7ad2d
WoongBin Choi <woongbin96@naver.com>
2022-07-12 19:14:30 +0900

    RestDoc 추가

---
+++ src/test/java/com/bincolog/api/controller/PostControllerDocTest.java
@@ -0,0 +1,10 @@
+package com.bincolog.api.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+//@ExtendWith(RestDocumentationExtension.class)
+public class PostControllerDocTest {
+
+}
