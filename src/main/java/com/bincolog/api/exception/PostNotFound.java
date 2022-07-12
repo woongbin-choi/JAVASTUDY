47601d24c4c12b68cfdf0633e65a37aa15adc656
WoongBin Choi <woongbin96@naver.com>
2022-07-12 12:59:24 +0900

    예외처리 추가

--- src/main/java/com/bincolog/api/exception/PostNotFound.java
+++ src/main/java/com/bincolog/api/exception/PostNotFound.java
@@ -1,6 +1,7 @@
 package com.bincolog.api.exception;
 
-public class PostNotFound extends RuntimeException{
+// status -> 404
+public class PostNotFound extends BincologException{
 
     private static final String MESSAGE = "존재하지 않는 글입니다";
 
@@ -8,7 +9,9 @@ public class PostNotFound extends RuntimeException{
         super(MESSAGE);
     }
 
-//    public PostNotFound(Throwable cause) {
-//        super(MESSAGE, cause);
-//    }
+    @Override
+    public int getStatusCode() {
+        return 404;
+    }
+
 }
