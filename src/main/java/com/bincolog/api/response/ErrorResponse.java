7358adb0c8157f2c4fd510de92565a054a9ea775
WoongBin Choi <woongbin96@naver.com>
2022-07-12 12:58:43 +0900

    예외처리 추가

--- src/main/java/com/bincolog/api/response/ErrorResponse.java
+++ src/main/java/com/bincolog/api/response/ErrorResponse.java
@@ -23,12 +23,13 @@ public class ErrorResponse {
 
     private final String code;
     private final String message;
-    private Map<String, String> validation = new HashMap<>();
+    private Map<String, String> validation;
 
     @Builder
-    public ErrorResponse(String code, String message) {
+    public ErrorResponse(String code, String message, Map<String, String> validation) {
         this.code = code;
         this.message = message;
+        this.validation = validation;
     }
 
     public void addValidation(String fieldName, String errorMessage){
