55e440394c0050d5ce7e606822a77f626f9aec40
WoongBin Choi <woongbin96@naver.com>
2022-07-12 12:59:18 +0900

    예외처리 추가

--- src/main/java/com/bincolog/api/request/PostCreate.java
+++ src/main/java/com/bincolog/api/request/PostCreate.java
@@ -1,5 +1,6 @@
 package com.bincolog.api.request;
 
+import com.bincolog.api.exception.InvalidRequest;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -28,4 +29,10 @@ public class PostCreate {
     // 가독성이 좋다 (값 생성에 대한 유연함)
     // 필요한 값만 받을 수 있다
     // 객체의 불변성
+
+    public void validationChk(){
+        if(title.contains("바보")){
+            throw new InvalidRequest("title", "제목에 바보를 포함할 수 없습니다.");
+        }
+    }
 }
