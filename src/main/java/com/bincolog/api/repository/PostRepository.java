47172ec49a0054800cda383c2ff72d3539b5a79b
WoongBin Choi <woongbin96@naver.com>
2022-07-18 13:48:00 +0900

    Business Logic Edit

--- src/main/java/com/bincolog/api/repository/PostRepository.java
+++ src/main/java/com/bincolog/api/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.bincolog.api.repository;
 
 import com.bincolog.api.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 public interface PostRepository extends JpaRepository<Post, Long>, PostRepositoryCustom {
 }
