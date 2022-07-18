747c00cf3b571d8f9e0667623f6c045417317053
WoongBin Choi <woongbin96@naver.com>
2022-07-18 13:47:15 +0900

    Vue.js Commit

---
+++ vue-cli/src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import { RouterLink, RouterView } from "vue-router";
+export default {
+  name: 'App',
+  components: {
+  }
+}
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
