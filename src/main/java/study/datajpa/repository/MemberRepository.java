a9b3bee440c01acaeaf43800788909cbb95e01e9
woongbin <woongbin96@naver.com>
2022-10-12 16:14:29 +0900

    update MemberRepository

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,19 @@
 package study.datajpa.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import study.datajpa.entity.Member;
 
 import java.util.List;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    // 조건이 3개 이상 될 시 복잡해지고 너무 길어지는 단점.
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
+
+
+    // 실무에서 많이 사용한다.
+    @Query("select m from Member m where m.username = :username and m.age = :age")
+    List<Member> findUser(@Param("username") String username, @Param("age") int age);
 }
