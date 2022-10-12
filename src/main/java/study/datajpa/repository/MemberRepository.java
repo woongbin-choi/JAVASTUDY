f143e81a3ab48ec650fe16f9a1189afa3e0e3b70
woongbin <woongbin96@naver.com>
2022-10-12 16:26:35 +0900

    update MemberRepository

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -13,7 +13,11 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findByUsernameAndAgeGreaterThan(String username, int age);
 
 
-    // 실무에서 많이 사용한다.
+    // 실무에서 많이 사용한다. (복잡한 정적 쿼리) -> 동적 쿼리는 QueryDSL을 사용
+    // 쿼리문 안에 프로퍼티 오타가 있을 때 애플리케이션 실행 시 오류로 잡아주는 장점이 있다
     @Query("select m from Member m where m.username = :username and m.age = :age")
     List<Member> findUser(@Param("username") String username, @Param("age") int age);
+
+    @Query("select m.username from Member m")
+    List<String> findUsernameList();
 }
