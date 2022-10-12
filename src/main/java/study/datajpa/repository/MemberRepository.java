8b63fbd8a3596c2a17180192127da663c51376d6
woongbin <woongbin96@naver.com>
2022-10-13 08:34:20 +0900

    update MemberRepository

--- src/main/java/study/datajpa/repository/MemberRepository.java
+++ src/main/java/study/datajpa/repository/MemberRepository.java
@@ -1,11 +1,15 @@
 package study.datajpa.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
 
+import java.awt.print.Pageable;
 import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
@@ -42,4 +46,46 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     List<Member> findListByUsername(String username); // 컬렉션 (데이터가 없을 경우에는 Empty 컬렉션을 반환함. Null이 아님)
     Optional<Member> findOptionalByUsername(String username); // 단건 (null 일수도 아닐수도 있음, 만약 결과가 2개 이상 나오면 예외 발생)
     Member findMemberByUsername(String username); // 단건 (데이터가 없을 경우에 Null을 반환)
+
+    // Paging (페이지는 0부터 시작한다)
+    // TotalCount가 데이터가 많을 시에 조회하는데 시간이 많이 걸릴 수 있다. JPA에서는 카운트 쿼리를 분리할 수 있게 제공한다
+    // 카운트 쿼리 분리 안할꺼면 @Query 빼도 된다.
+    // 정렬도 복잡한 정렬이 필요한 경우 카운트 쿼리처럼 따로 빼야 한다.
+    @Query(value = "select m from Member m",
+            countQuery = "select count(m.username) from Member m")
+    Page<Member> findByAge(int age, Pageable pageable);
+
+    /*
+     벌크성 수정 쿼리 (@Modifying 어노테이션이 있어야 업데이트가 실행된다.)
+     영속성 컨텍스트와 관계없이 바로 DB에 적용하기 때문에 영속성 컨텍스트를 날려야한다. 안그러면 영속성 컨텍스트에는 값이 그대로고 DB값만 바뀌어서 문제가 생긴다.
+     테스트는 순수 JPA와 똑같이 작동
+     */
+    @Modifying(clearAutomatically = true)
+    @Query("update Member m set m.age = m.age + 1 where m.age >= :age")
+    int bulkAgePlus(@Param("age") int age);
+
+
+    /*
+      fetch join
+     */
+    @Query("select m from Member m left join fetch m.team")
+    List<Member> findMemberFetchJoin();
+
+
+    /*
+     Entity Graph(사실상 fetch join의 간편 버전) - 밑에 3개 다 똑같은 기능이긴 하다.
+     */
+    //공통 메서드 오버라이드
+    @Override
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findAll();
+
+    //JPQL + 엔티티 그래프
+    @EntityGraph(attributePaths = {"team"})
+    @Query("select m from Member m")
+    List<Member> findMemberEntityGraph();
+
+    //메서드 이름으로 쿼리에서 특히 편리하다.
+    @EntityGraph(attributePaths = {"team"})
+    List<Member> findEntityGraphByUsername(@Param("username") String username);
 }
