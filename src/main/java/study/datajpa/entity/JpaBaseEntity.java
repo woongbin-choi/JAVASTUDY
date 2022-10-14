bdaa453291420077aa4b853948103ef3e53e2efa
woongbin <woongbin96@naver.com>
2022-10-14 12:15:03 +0900

    Add JpaBaseEntity

---
+++ src/main/java/study/datajpa/entity/JpaBaseEntity.java
@@ -0,0 +1,24 @@
+package study.datajpa.entity;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+public class JpaBaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdDate;
+    private LocalDateTime updatedDate;
+
+    @PrePersist
+    public void prePersist() {
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        updatedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate() {
+        updatedDate = LocalDateTime.now();
+    }
+}
