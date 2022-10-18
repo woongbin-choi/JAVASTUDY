7032e86caa97e80a929039d65867eb3d3f26ec34
woongbin <woongbin96@naver.com>
2022-10-18 17:50:46 +0900

    fetch type

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -98,14 +98,10 @@ public class QuerydslBasicTest {
         List<Member> fetch = queryFactory
                 .selectFrom(member)
                 .fetch();
-        //단 건
-        Member findMember1 = queryFactory
-                .selectFrom(member)
-                .fetchOne();
+
         //처음 한 건 조회
         Member findMember2 = queryFactory
                 .selectFrom(member)
                 .fetchFirst();
-
     }
 }
