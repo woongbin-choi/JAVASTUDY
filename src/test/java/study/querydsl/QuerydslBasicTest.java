a7f4b40c9d70f60a197812d1518e93bcd13299bb
woongbin <woongbin96@naver.com>
2022-10-25 08:24:18 +0900

    concat

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -2,6 +2,7 @@ package study.querydsl;
 
 import com.querydsl.core.Tuple;
 import com.querydsl.core.types.dsl.CaseBuilder;
+import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQuery;
 import com.querydsl.jpa.impl.JPAQueryFactory;
@@ -373,7 +374,8 @@ public class QuerydslBasicTest {
                 .select(member.age
                         .when(10).then("열살")
                         .when(20).then("20살")
-                        .otherwise("기타"))
+                        .otherwise("기타")
+                )
                 .from(member)
                 .fetch();
 
@@ -396,5 +398,31 @@ public class QuerydslBasicTest {
         for (String s : result) {
             System.out.println(s);
         }
+    }
+
+    @Test
+    public void constant() {
+        List<Tuple> result = queryFactory
+                .select(member.username, Expressions.constant("A"))
+                .from(member)
+                .fetch();
+
+        for (Tuple tuple : result) {
+            System.out.println("tuple = " + tuple);
+        }
+    }
+
+    @Test
+    public void concat() {
+        List<String> result = queryFactory
+                .select(member.username.concat("_").concat(member.age.stringValue()))
+                .from(member)
+                .where(member.username.eq("member1"))
+                .fetch();
+
+        for (String s : result) {
+            System.out.println(s);
+        }
+
     }
 }
