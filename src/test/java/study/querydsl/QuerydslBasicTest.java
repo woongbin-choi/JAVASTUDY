9058f4197fa974ce185a23f04a5b0ad58e65cb40
woongbin <woongbin96@naver.com>
2022-11-14 12:17:49 +0900

    simple projection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -423,4 +423,18 @@ public class QuerydslBasicTest {
         }
 
     }
+
+    @Test
+    public void simpleProjection() {
+        List<String> result = queryFactory
+                .select(member.username)
+                .from(member)
+                .fetch();
+
+        for(String s : result) {
+            System.out.println("s ="+ s);
+        }
+    }
+
+
 }
