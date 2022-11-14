2a6bedf9046dae246d7266cdb91ad11eff387f3d
woongbin <woongbin96@naver.com>
2022-11-14 12:26:59 +0900

    setter 방식 사용

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -466,5 +466,17 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void findDtoBySetter() {
+        List<MemberDto> result = queryFactory
+                .select(Projections.bean(MemberDto.class,
+                        member.username,
+                        member.age))
+                .from(member)
+                .fetch();
 
+        for (MemberDto memberDto : result) {
+            System.out.println("memberDto =" + memberDto);
+        }
+    }
 }
