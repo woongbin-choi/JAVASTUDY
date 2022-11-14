8bdd0919150d379cf260b5df274a0b7d010985e9
woongbin <woongbin96@naver.com>
2022-11-14 12:18:15 +0900

    tuple projection

--- src/test/java/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/study/querydsl/QuerydslBasicTest.java
@@ -436,5 +436,18 @@ public class QuerydslBasicTest {
         }
     }
 
+    @Test
+    public void tupleProjection() {
+        List<Tuple> result = queryFactory
+                .select(member.username, member.age)
+                .from(member)
+                .fetch();
 
+        for (Tuple tuple : result) {
+            String username = tuple.get(member.username);
+            Integer age = tuple.get(member.age);
+            System.out.println("username =" + username);
+            System.out.println("age =" + age);
+        }
+    }
 }
