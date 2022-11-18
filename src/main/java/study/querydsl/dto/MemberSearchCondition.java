41ef39d1ef3fb140f3c01ec419865da3179c4140
woongbin <woongbin96@naver.com>
2022-11-18 14:24:47 +0900

    create MemberSearchCondition

---
+++ src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -0,0 +1,12 @@
+package study.querydsl.dto;
+
+import lombok.Data;
+
+@Data
+public class MemberSearchCondition {
+
+    private String username;
+    private String teamName;
+    private Integer ageGoe; // 나이 크거나 같거나
+    private Integer ageLoe; // 나이 작거나 같거나
+}
