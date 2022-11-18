3100f2f151a242b298176ecc1cae3cf3e8ab3671
woongbin <woongbin96@naver.com>
2022-11-18 17:05:43 +0900

    update MemberSearchCondition

--- src/main/java/study/querydsl/dto/MemberSearchCondition.java
+++ src/main/java/study/querydsl/dto/MemberSearchCondition.java
@@ -6,6 +6,6 @@ import lombok.Data;
 public class MemberSearchCondition {
     private String username;
     private String teamName;
-    private Integer ageGoe; // 나이 크거나 같거나
-    private Integer ageLoe; // 나이 작거나 같거나
+    private Integer ageGoe;
+    private Integer ageLoe;
 }
